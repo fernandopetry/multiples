889f2194daa9cf74971a065efc6de0297bd51924
Fernando Petry <fernando@ideia.ppg.br>
2017-06-25 14:01:30 -0300

    Limpa codigo

--- index.php
+++ index.php
@@ -2,19 +2,11 @@
 
 require_once './vendor/autoload.php';
 
-//use Petry\Multiples\MultiplesFacade;
-//
-//try {
-//    $facade = new MultiplesFacade(1, 100);
-//    echo $facade->getPrint();
-//} catch (\Exception $e) {
-//    echo $e->getMessage();
-//}
+use Petry\Multiples\MultiplesFacade;
 
-for ($i = 1; $i <= 100; $i++) {
-    if (($i % 3) == 0) {
-        $print = new Petry\Multiples\PrintsMultiples($i);
-        $print->addRules(new Petry\Multiples\Rules\Multiple3());
-        echo $print->run() . $i . '<br>';
-    }
+try {
+    $facade = new MultiplesFacade(1, 100);
+    echo $facade->getPrint();
+} catch (\Exception $e) {
+    echo $e->getMessage();
 }
